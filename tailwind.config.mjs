572d2dd2399e655c7d5a3892d3d3f229a11243c6
rafopm <rafopm@gmail.com>
2024-11-22 13:28:43 -0500

    Fix GitHub Actions config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		typography: {
+			DEFAULT: {
+
+				css: {
+					h2: {
+						color: 'var(--color-900)',
+						marginBottom: '0 !important',
+					},
+					h3: {
+						color: 'var(--color-900)',
+						marginBottom: '0',
+					},
+					strong: {
+						color: 'var(--color-900)',
+					},
+					a: {
+						color: 'var(--color-700)',
+						'&:hover': {
+							color: 'var(--color-500)',
+						},
+
+					},
+
+				},
+
+			},
+
+		},
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		 // ...
-	   ],
+	plugins: [typography]
 }
