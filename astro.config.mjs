f6383d28be7e7a9a4ba802a9e807db156838244d
rafopm <rafopm@gmail.com>
2024-11-22 17:13:18 -0500

    update prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/cursosvg/',
   site: 'https://rafopm.github.io/cursosvg/',
   output: 'static', 
   
